abf45d7c1d690ac48d1416d78d111138c1bf3bf2
dacedast <ryudast@gmail>
2022-02-24 19:56:14 +0100

    fonts2

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { Layout } from "../components";
 import "../styles/globals.scss";
 import Router from "next/router";
 import { Loader } from "../components";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   const [loading, setLoading] = useState(false);
@@ -14,6 +15,12 @@ function MyApp({ Component, pageProps }) {
   });
   return (
     <Layout>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
       {loading && <Loader />}
       <Component {...pageProps} />
     </Layout>
